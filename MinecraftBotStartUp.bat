c86810086d7fa6d9565e01b09bfd5e07a36879dd
PSYmoom <saadsymoom@gmail.com>
2020-09-05 08:33:05 -0400

    Added the startup bat file and made some readability changes.

---
+++ MinecraftBotStartUp.bat
@@ -0,0 +1,3 @@
+//Sample MinecraftBotStartUp.bat file; MAKE THE REQUIRED CHANGES AND DELETE THIS LINE!
+cd <location of your Discord Bot>
+node MinecraftBot.js
--- server_start.bat
+++ server_start.bat
@@ -1,3 +1,3 @@
-//Sample .bat file
-cd /d D:\Minecraft_Discord_Playthrough
-java -Xmx12G -Xms1024M -jar server.jar nogui
+//Sample server_start.bat file; MAKE THE REQUIRED CHANGES AND DELETE THIS LINE!
+cd <location of your minecraft server>
+java -Xmx12G -Xms1024M -jar <server name> nogui
